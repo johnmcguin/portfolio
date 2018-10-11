12b14f00cb06eaf13244048272b37ae8cc2059a4
John <johnmcguin88@gmail.com>
2018-10-11 16:12:22 -0600

    remove comment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,6 @@ class Footer extends React.Component {
                         <li><a href="https://www.linkedin.com/in/johnmcguin/" target="_blank" className="icon fa-linkedin"><span className="label">Github</span></a></li>
                         <li><a href="mailto:johnmcguin88@gmail.com?Subject=Let&#39;s%20Do%20Work!" target="_top" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
-                    {/* <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul> */}
                 </div>
             </div>
         )
